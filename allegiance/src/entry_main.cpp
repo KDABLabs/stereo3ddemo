9e85a0925d4c1301cea72cd63bbaae10427565f1
Mike Krus <mike.krus@kdab.com>
2023-09-18 11:17:43 +0100

    Force Qt3D to use GL
    Change-Id: I9601bab37bf77a5b815972429f5330e11b777444
    Reviewed-on: https://codereview.kdab.com/c/schneider-digital/qt3d/+/130882
    Reviewed-by: Ilya Doroshenko <ilya.doroshenko@kdab.com>

--- allegiance/src/entry_main.cpp
+++ allegiance/src/entry_main.cpp
@@ -2,7 +2,7 @@
 
 int main(int argc, char** argv)
 {
-//    qputenv("QT3D_RENDERER", "opengl");
+    qputenv("QT3D_RENDERER", "opengl");
 //    qputenv("QSG_INFO", "true");
 //    qputenv("QT_LOGGING_RULES", "qt.rhi.general.debug=true");
     return App{ argc, argv }.Start();
