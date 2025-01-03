4a53e212459eab5af9019f8634f5f23fa9699fb4
Paul Lemire <paul.lemire@kdab.com>
2025-01-03 12:31:48 +0100

    Rename camera_control.qml to side_menu.qml
    Change-Id: I48b639145d54e2f77a969c228a2028248916346c
    Reviewed-on: https://codereview.kdab.com/c/schneider-digital/allegiance/+/150230
    Tested-by: Continuous Integration <build@kdab.com>
    Reviewed-by: Mike Krus <mike.krus@kdab.com>

--- allegiance/applications/qt/common/side_menu.h
+++ allegiance/applications/qt/common/side_menu.h
@@ -25,7 +25,7 @@ public:
 
         m_engine = new QQmlEngine(this);
         m_quickWidget = new QQuickWidget(m_engine, this);
-        m_quickWidget->setSource(QUrl(QStringLiteral("qrc:/qt/qml/common/qml/camera_control.qml")));
+        m_quickWidget->setSource(QUrl(QStringLiteral("qrc:/qt/qml/common/qml/side_menu.qml")));
         m_quickWidget->setResizeMode(QQuickWidget::SizeRootObjectToView);
         m_quickWidget->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
 
